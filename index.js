7c23190707ddc6cc1fcf02e827ee149cfeb1768d
w1waalx <154117370+kiing400@users.noreply.github.com>
2024-11-23 00:05:26 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+// function openForm() {
+//     document.getElementById("myForm").style.display = "block";
+// }
+
+// function closeForm() {
+//     document.getElementById("myForm").style.display = "none";
+// }
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
